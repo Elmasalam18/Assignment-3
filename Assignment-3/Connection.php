85db4751d0d41f0388c02ffa943c7b250c11f4bb
Elmasalam18 <90953271+Elmasalam18@users.noreply.github.com>
2023-11-22 06:47:57 -1200

    Add files via upload

---
+++ Assignment-3/Connection.php
@@ -0,0 +1,13 @@
+<?php
+$HOSTNAME='localhost';
+$USERNAME='root';
+$PASSWORD='';
+$DATABASE='test';
+
+$conn=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
+
+if($conn->connect_error){
+    die('Connection Failed:'. $conn->connect_error);
+}
+
+?>
